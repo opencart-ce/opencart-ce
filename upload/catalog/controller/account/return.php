d99c28191c852ddf0f3263e353f6836d11df614c
John Sheppard <john@addevelopments.com>
2013-07-12 11:41:17 +0100

    Fix ColorBox on return page
    https://github.com/opencart/opencart/commit/b8a65ef459d7bd9bf0109620810ea1c92386c2fc
    
    https://github.com/opencart/opencart/issues/448

--- upload/catalog/controller/account/return.php
+++ upload/catalog/controller/account/return.php
@@ -12,8 +12,6 @@ class ControllerAccountReturn extends Controller {
 		$this->language->load('account/return');
 
 		$this->document->setTitle($this->language->get('heading_title'));
-		$this->document->addScript('catalog/view/javascript/jquery/colorbox/jquery.colorbox-min.js');
-		$this->document->addStyle('catalog/view/javascript/jquery/colorbox/colorbox.css');
 
 		$this->data['breadcrumbs'] = array();
 
@@ -308,6 +306,9 @@ class ControllerAccountReturn extends Controller {
 		}
 
 		$this->document->setTitle($this->language->get('heading_title'));
+
+		$this->document->addScript('catalog/view/javascript/jquery/colorbox/jquery.colorbox-min.js');
+		$this->document->addStyle('catalog/view/javascript/jquery/colorbox/colorbox.css');
 
 		$this->data['breadcrumbs'] = array();
 
