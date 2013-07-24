fbe070ae854e815507802acc90b35a943b794415
opencarthelp <git@opencarthelp.com>
2013-07-24 12:22:29 -0500

    Removed incomplete extension manager

--- upload/admin/language/english/common/header.php
+++ upload/admin/language/english/common/header.php
@@ -35,7 +35,6 @@ $_['text_localisation']                = 'Localisation';
 $_['text_logged']                      = 'You are logged in as <span>%s</span>';
 $_['text_logout']                      = 'Logout';
 $_['text_contact']                     = 'Mail';
-$_['text_manager']                     = 'Extension Manager';
 $_['text_manufacturer']                = 'Manufacturers';
 $_['text_module']                      = 'Modules';
 $_['text_option']                      = 'Options';
