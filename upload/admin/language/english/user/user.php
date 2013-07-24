a4c3e8ff0df2d0436473113d7d2dc53cacf1d075
opencarthelp <git@opencarthelp.com>
2013-07-24 14:04:51 -0500

    Removed unused admin user setup captcha

--- upload/admin/language/english/user/user.php
+++ upload/admin/language/english/user/user.php
@@ -20,7 +20,6 @@ $_['entry_lastname']   = 'Last Name:';
 $_['entry_email']      = 'E-Mail:';
 $_['entry_user_group'] = 'User Group:';
 $_['entry_status']     = 'Status:';
-$_['entry_captcha']    = 'Enter the code in the box below:';
 
 // Error
 $_['error_permission'] = 'Warning: You do not have permission to modify users!';
@@ -31,5 +30,4 @@ $_['error_password']   = 'Password must be between 4 and 20 characters!';
 $_['error_confirm']    = 'Password and password confirmation do not match!';
 $_['error_firstname']  = 'First Name must be between 1 and 32 characters!';
 $_['error_lastname']   = 'Last Name must be between 1 and 32 characters!';
-$_['error_captcha']    = 'Verification code does not match the image!';
 ?>
