85184bac160811faf5be1261bf5697e423c70b21
opencarthelp <git@opencarthelp.com>
2013-08-12 18:06:15 -0500

    Whitespace standardization

--- upload/catalog/controller/common/maintenance.php
+++ upload/catalog/controller/common/maintenance.php
@@ -1,7 +1,7 @@
 <?php
 class ControllerCommonMaintenance extends Controller {
-    public function index() {
-        if ($this->config->get('config_maintenance')) {
+	public function index() {
+		if ($this->config->get('config_maintenance')) {
 			$route = '';
 
 			if (isset($this->request->get['route'])) {
@@ -20,31 +20,31 @@ class ControllerCommonMaintenance extends Controller {
 			if (($route != 'payment') && !$this->user->isLogged()) {
 				return $this->forward('common/maintenance/info');
 			}
-        }
-    }
+		}
+	}
 
 	public function info() {
-        $this->language->load('common/maintenance');
+		$this->language->load('common/maintenance');
 
-        $this->document->setTitle($this->language->get('heading_title'));
+		$this->document->setTitle($this->language->get('heading_title'));
 
-        $this->data['heading_title'] = $this->language->get('heading_title');
+		$this->data['heading_title'] = $this->language->get('heading_title');
 
-        $this->document->breadcrumbs = array();
+		$this->document->breadcrumbs = array();
 
-        $this->document->breadcrumbs[] = array(
-            'text'      => $this->language->get('text_maintenance'),
+		$this->document->breadcrumbs[] = array(
+			'text'      => $this->language->get('text_maintenance'),
 			'href'      => $this->url->link('common/maintenance'),
-            'separator' => false
-        );
+			'separator' => false
+		);
 
-        $this->data['message'] = $this->language->get('text_message');
+		$this->data['message'] = $this->language->get('text_message');
 
 		if (file_exists(DIR_TEMPLATE . $this->config->get('config_template') . '/template/common/maintenance.tpl')) {
-            $this->template = $this->config->get('config_template') . '/template/common/maintenance.tpl';
-        } else {
-            $this->template = 'default/template/common/maintenance.tpl';
-        }
+			$this->template = $this->config->get('config_template') . '/template/common/maintenance.tpl';
+		} else {
+			$this->template = 'default/template/common/maintenance.tpl';
+		}
 
 		$this->children = array(
 			'common/footer',
@@ -52,6 +52,6 @@ class ControllerCommonMaintenance extends Controller {
 		);
 
 		$this->response->setOutput($this->render());
-    }
+	}
 }
 ?>
