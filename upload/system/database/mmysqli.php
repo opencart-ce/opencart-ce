01fcf8c209cbdb58343a9e292c1717d735885f56
opencarthelp <git@opencarthelp.com>
2013-08-18 16:43:30 -0500

    Switched MySQLi to fetch_assoc()
    since numbered indices aren't used.
    
    https://github.com/opencart/opencart/pull/812

--- upload/system/database/mmysqli.php
+++ upload/system/database/mmysqli.php
@@ -25,7 +25,7 @@ final class mMySQLi {
 				$result = new stdClass();
 				$data = array();
 
-				while ($row = $query->fetch_array()) {
+				while ($row = $query->fetch_assoc()) {
 					$data[] = $row;
 				}
 
