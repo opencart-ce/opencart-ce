85825113f1cea109a100b5adec554a9330b96277
John Sheppard <john@addevelopments.com>
2013-09-03 12:41:47 +0100

    Add missing SSL to breadcrumb URLs

--- upload/admin/controller/common/forgotten.php
+++ upload/admin/controller/common/forgotten.php
@@ -55,7 +55,7 @@ class ControllerCommonForgotten extends Controller {
 
 		$this->data['breadcrumbs'][] = array(
 			'text'      => $this->language->get('text_home'),
-			'href'      => $this->url->link('common/home'),
+			'href'      => $this->url->link('common/home', '', 'SSL'),
 			'separator' => false
 		);
 
--- upload/admin/controller/common/reset.php
+++ upload/admin/controller/common/reset.php
@@ -36,7 +36,7 @@ class ControllerCommonReset extends Controller {
 
 			$this->data['breadcrumbs'][] = array(
 				'text'      => $this->language->get('text_home'),
-				'href'      => $this->url->link('common/home'),
+				'href'      => $this->url->link('common/home', '', 'SSL'),
 				'separator' => false
 			);
 
