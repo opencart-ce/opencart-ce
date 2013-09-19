b796ac64a3f7d87d7fbade0a55aee2231c3be694
opencarthelp <git@opencarthelp.com>
2013-09-18 22:11:34 -0500

    Admin validation correction

--- upload/admin/controller/common/login.php
+++ upload/admin/controller/common/login.php
@@ -99,7 +99,7 @@ class ControllerCommonLogin extends Controller {
 	}
 
 	protected function validate() {
-		if (isset($this->request->post['username']) && isset($this->request->post['password']) && !$this->user->login($this->request->post['username'], $this->request->post['password'])) {
+		if (!isset($this->request->post['username']) || !isset($this->request->post['password']) || !$this->user->login($this->request->post['username'], $this->request->post['password'])) {
 			$this->error['warning'] = $this->language->get('error_login');
 		}
 
