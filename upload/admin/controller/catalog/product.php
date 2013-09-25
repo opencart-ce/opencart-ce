86af946aa4a3374da2a0d607ac7dd14bd13420ee
opencarthelp <git@opencarthelp.com>
2013-09-25 14:38:27 -0500

    Updated product form validation to match error
    Error text also updated to make limits clearer for user

--- upload/admin/controller/catalog/product.php
+++ upload/admin/controller/catalog/product.php
@@ -1259,12 +1259,12 @@ class ControllerCatalogProduct extends Controller {
 		}
 
 		foreach ($this->request->post['product_description'] as $language_id => $value) {
-			if ((utf8_strlen($value['name']) < 1) || (utf8_strlen($value['name']) > 255)) {
+			if ((utf8_strlen($value['name']) < 3) || (utf8_strlen($value['name']) > 255)) {
 				$this->error['name'][$language_id] = $this->language->get('error_name');
 			}
 		}
 
-		if ((utf8_strlen($this->request->post['model']) < 1) || (utf8_strlen($this->request->post['model']) > 64)) {
+		if ((utf8_strlen($this->request->post['model']) < 3) || (utf8_strlen($this->request->post['model']) > 64)) {
 			$this->error['model'] = $this->language->get('error_model');
 		}
 
--- upload/admin/language/english/catalog/product.php
+++ upload/admin/language/english/catalog/product.php
@@ -78,6 +78,6 @@ $_['entry_layout']           = 'Layout Override:';
 // Error
 $_['error_warning']          = 'Warning: Please check the form carefully for errors!';
 $_['error_permission']       = 'Warning: You do not have permission to modify products!';
-$_['error_name']             = 'Product Name must be greater than 3 and less than 255 characters!';
-$_['error_model']            = 'Product Model must be greater than 3 and less than 64 characters!';
+$_['error_name']             = 'Product Name must be between 3 and 255 characters!';
+$_['error_model']            = 'Product Model must be between 3 and 64 characters!';
 ?>
