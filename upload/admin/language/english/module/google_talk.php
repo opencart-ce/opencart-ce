73380ca254119b766c513fd231cb0b26d5a665a4
opencarthelp <git@opencarthelp.com>
2013-09-25 22:56:41 -0500

    Closes #34 Google Talk discontinued
    Replaced with a generic "Live Chat" module.  Filenames remain
    google_talk.* to keep compatible with non-English language files.

--- upload/admin/language/english/module/google_talk.php
+++ upload/admin/language/english/module/google_talk.php
@@ -1,23 +1,23 @@
 <?php
 // Heading
-$_['heading_title']       = 'Google Talk';
+$_['heading_title']       = 'Live Chat (Formerly Google Talk)';
 
 // Text
 $_['text_module']         = 'Modules';
-$_['text_success']        = 'Success: You have modified module Google Talk!';
+$_['text_success']        = 'Success: You have modified module Live Chat!';
 $_['text_content_top']    = 'Content Top';
 $_['text_content_bottom'] = 'Content Bottom';
 $_['text_column_left']    = 'Column Left';
 $_['text_column_right']   = 'Column Right';
 
 // Entry
-$_['entry_code']          = 'Google Talk Code:<br /><span class="help">Goto <a href="http://www.google.com/talk/service/badge/New" target="_blank"><u>Create a Google Talk chatback badge</u></a> and copy &amp; paste the generated code into the text box.</span>';
+$_['entry_code']          = 'Live Chat Code:<br /><span class="help">Copy and paste the HTML code to generate the chat box. (Google Talk has been discontinued and will no longer function.)</span>';
 $_['entry_layout']        = 'Layout:';
 $_['entry_position']      = 'Position:';
 $_['entry_status']        = 'Status:';
 $_['entry_sort_order']    = 'Sort Order:';
 
 // Error
-$_['error_permission']    = 'Warning: You do not have permission to modify module Google Talk!';
+$_['error_permission']    = 'Warning: You do not have permission to modify module Live Chat!';
 $_['error_code']          = 'Code Required';
 ?>
