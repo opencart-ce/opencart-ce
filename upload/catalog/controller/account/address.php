c178112fa3fcde6b0794254d61af0a0615af40d2
John Sheppard <john@addevelopments.com>
2013-12-23 12:42:06 +0000

    country_id should of been zone_id in address

--- upload/catalog/controller/account/address.php
+++ upload/catalog/controller/account/address.php
@@ -602,7 +602,7 @@ class ControllerAccountAddress extends Controller {
 			$this->error['country'] = $this->language->get('error_country');
 		}
 
-		if (!isset($this->request->post['zone_id']) || $this->request->post['zone_id'] == '' || !is_numeric($this->request->post['country_id'])) {
+		if (!isset($this->request->post['zone_id']) || $this->request->post['zone_id'] == '' || !is_numeric($this->request->post['zone_id'])) {
 			$this->error['zone'] = $this->language->get('error_zone');
 		}
 
