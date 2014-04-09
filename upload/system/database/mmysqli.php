2f1c95a2bf2fc34e615cc77ed2f4dee3044184f4
opencarthelp <git@opencarthelp.com>
2014-04-09 17:30:53 -0500

    Backported most recent MySQLi driver
    INSERT/UPDATE/etc. now return bool true like other DB drivers, error
    redundancy removed

--- upload/system/database/mmysqli.php
+++ upload/system/database/mmysqli.php
@@ -1,73 +1,58 @@
 <?php
 final class mMySQLi {
-	private $mysqli;
+	private $link;
 
 	public function __construct($hostname, $username, $password, $database) {
-		$this->mysqli = new mysqli($hostname, $username, $password, $database);
+		$this->link = new mysqli($hostname, $username, $password, $database);
 
-		if ($this->mysqli->connect_error) {
-			trigger_error('Error: Could not make a database link (' . $this->mysqli->connect_errno . ') ' . $this->mysqli->connect_error);
+		if ($this->link->connect_error) {
+			trigger_error('Error: Could not make a database link (' . $this->link->connect_errno . ') ' . $this->link->connect_error);
 		}
 
-		$this->mysqli->set_charset('utf8');
+		$this->link->set_charset('utf8');
 	}
 
 	public function query($sql) {
-		$query = $this->mysqli->query($sql);
+		$query = $this->link->query($sql);
 
-		if ($this->mysqli->errno) {
-			trigger_error('Error: ' . $this->mysqli->error . '<br />Error No: ' . $this->mysqli->errno . '<br />' . $sql);
-			exit();
-		}
-
-		if ($query) {
-			if (isset($query->num_rows)) {
-				$result = new stdClass();
+		if (!$this->link->errno) {
+			if ($query instanceof mysqli_result) {
 				$data = array();
 
 				while ($row = $query->fetch_assoc()) {
 					$data[] = $row;
 				}
 
+				$result = new stdClass();
 				$result->row = isset($data[0]) ? $data[0] : array();
 				$result->rows = $data;
 				$result->num_rows = $query->num_rows;
 
 				$query->close();
 
-				unset($data);
-
 				return $result;
 			} else {
-				// When MySQLi returns boolean true instead of a result object
-				// http://www.php.net/manual/en/mysqli.query.php
-				$result = new stdClass();
-				$result->row = array();
-				$result->rows = array();
-				$result->num_rows = 0;
-
-				return $result;
+				return true;
 			}
 		} else {
-			trigger_error('Error: ' . $this->mysqli->error . '<br />Error No: ' . $this->mysqli->errno . '<br />' . $sql);
-			exit();
+			trigger_error('Error: ' . $this->link->error  . '<br />Error No: ' . $this->link->errno . '<br />' . $sql);
 		}
 	}
 
 	public function escape($value) {
-		return $this->mysqli->real_escape_string($value);
+		return $this->link->real_escape_string($value);
 	}
 
 	public function countAffected() {
-		return $this->mysqli->affected_rows;
+		return $this->link->affected_rows;
 	}
 
 	public function getLastId() {
-		return $this->mysqli->insert_id;
+		return $this->link->insert_id;
 	}
 
 	public function __destruct() {
-		$this->mysqli->close();
+		$this->link->close();
 	}
 }
 ?>
