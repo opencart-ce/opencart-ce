346780388bcbc15000ce039c764ecfe5deaa1ded
opencarthelp <git@opencarthelp.com>
2014-06-12 15:51:14 -0500

    Remove admin forgotten password redirect
    Prevents unauthorized access to admin when a user doesn't log out
    properly (i.e. closing the browser tab).

--- upload/admin/controller/common/forgotten.php
+++ upload/admin/controller/common/forgotten.php
@@ -3,10 +3,6 @@ class ControllerCommonForgotten extends Controller {
 	private $error = array();
 
 	public function index() {
-		if ($this->user->isLogged() && isset($this->session->data['token'])) {
-			$this->redirect($this->url->link('common/home', 'token=' . $this->session->data['token'], 'SSL'));
-		}
-
 		if (!$this->config->get('config_password')) {
 			$this->redirect($this->url->link('common/login', '', 'SSL'));
 		}
