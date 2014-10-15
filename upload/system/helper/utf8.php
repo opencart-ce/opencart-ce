a32b16beebd0140462343f390494e0a767a15cdf
John Sheppard <john@addevelopments.com>
2014-10-15 18:02:11 +0100

    Changed UTF8 helper license/attribution #70

--- upload/system/helper/utf8.php
+++ upload/system/helper/utf8.php
@@ -1,56 +1,23 @@
 <?php
 /*
- * Based on PHP-UTF8 by Henri Sivonen.
- * http://iki.fi/hsivonen/php-utf8/
- */
- 
-/* ***** BEGIN LICENSE BLOCK *****
- * Version: NPL 1.1/GPL 2.0/LGPL 2.1
+ * Parts of the contents of this file are based on work by other authors and
+ * have been used and modified under the terms of the GNU General Public
+ * License version 3 or later.
  *
- * The contents of this file are subject to the Netscape Public License
- * Version 1.1 (the "License"); you may not use this file except in
- * compliance with the License. You may obtain a copy of the License at
- * http://www.mozilla.org/NPL/
+ * Based on core.php from the PHP UTF-8 library by Harry Fuecks.
+ * http://sourceforge.net/projects/phputf8/
+ * Which has contributions from:
+ * <chernyshevsky at hotmail dot com>
+ * Chris Smith
+ * Andreas Gohr
  *
- * Software distributed under the License is distributed on an "AS IS" basis,
- * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
- * for the specific language governing rights and limitations under the
- * License.
- *
- * The Original Code is Mozilla Communicator client code.
- *
- * The Initial Developer of the Original Code is
- * Netscape Communications Corporation.
- * Portions created by the Initial Developer are Copyright (C) 1998
+ * The functions utf8_to_unicode and utf8_from_unicode are based a PHP port by
+ * Henri Sivonen. http://iki.fi/hsivonen/php-utf8/
+ * The Initial Developer of the Original Code is Netscape Communications
+ * Corporation. Portions created by the Initial Developer are Copyright (C) 1998
  * the Initial Developer. All Rights Reserved.
- *
- * Contributor(s):
- * Henri Sivonen, hsivonen@iki.fi
- *
- *
- * Alternatively, the contents of this file may be used under the terms of
- * either the GNU General Public License Version 2 or later (the "GPL"), or
- * the GNU Lesser General Public License Version 2.1 or later (the "LGPL"),
- * in which case the provisions of the GPL or the LGPL are applicable instead
- * of those above. If you wish to allow use of your version of this file only
- * under the terms of either the GPL or the LGPL, and not to allow others to
- * use your version of this file under the terms of the NPL, indicate your
- * decision by deleting the provisions above and replace them with the notice
- * and other provisions required by the GPL or the LGPL. If you do not delete
- * the provisions above, a recipient may use your version of this file under
- * the terms of any one of the NPL, the GPL or the LGPL.
- *
- * ***** END LICENSE BLOCK ***** */
-
-/*
- * For the original C++ code, see
  * http://lxr.mozilla.org/seamonkey/source/intl/uconv/src/nsUTF8ToUnicode.cpp
  * http://lxr.mozilla.org/seamonkey/source/intl/uconv/src/nsUnicodeToUTF8.cpp
- *
- * The latest version of this file can be obtained from
- * http://iki.fi/hsivonen/php-utf8/
- *
- * Version 1.0, 2003-05-30
  */
 
 function utf8_strlen($string) {
