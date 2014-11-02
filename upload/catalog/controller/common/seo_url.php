2a0e2a915b17e8fc133c1dd04d0ef8afc4dab158
John Sheppard <john@addevelopments.com>
2014-11-02 12:52:49 +0000

    Fix for SEO URL ampersand encoding
    https://github.com/opencart/opencart/pull/1802

--- upload/catalog/controller/common/seo_url.php
+++ upload/catalog/controller/common/seo_url.php
@@ -112,7 +112,7 @@ class ControllerCommonSeoUrl extends Controller {
 					}
 
 					if ($query) {
-						$query = '?' . trim($query, '&');
+						$query = '?' . str_replace('&', '&amp;', trim($query, '&'));
 					}
 				}
 
