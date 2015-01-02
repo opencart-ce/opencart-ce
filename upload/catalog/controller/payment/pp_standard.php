8e5ec560054f42b467ddfbdcf40f3881cacb1273
John Sheppard <john@addevelopments.com>
2015-01-02 18:09:08 +0000

    Prevent security warning on return from PayPal
    Seems to only affect Firefox.
    http://forum.opencart.com/viewtopic.php?t=34654

--- upload/catalog/controller/payment/pp_standard.php
+++ upload/catalog/controller/payment/pp_standard.php
@@ -85,7 +85,7 @@ class ControllerPaymentPPStandard extends Controller {
 			$this->data['email'] = $order_info['email'];
 			$this->data['invoice'] = $this->session->data['order_id'] . ' - ' . html_entity_decode($order_info['payment_firstname'], ENT_QUOTES, 'UTF-8') . ' ' . html_entity_decode($order_info['payment_lastname'], ENT_QUOTES, 'UTF-8');
 			$this->data['lc'] = $this->session->data['language'];
-			$this->data['return'] = $this->url->link('checkout/success');
+			$this->data['return'] = $this->url->link('checkout/success', '', 'SSL');
 			$this->data['notify_url'] = $this->url->link('payment/pp_standard/callback', '', 'SSL');
 			$this->data['cancel_return'] = $this->url->link('checkout/checkout', '', 'SSL');
 
