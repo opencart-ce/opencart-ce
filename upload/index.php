5e10e4502753d8ff6c84576fb6931db03d98144e
John Sheppard <2039130+ADDCreative@users.noreply.github.com>
2018-06-06 12:51:26 +0100

    Don't handle errors suppressed with @
    https://github.com/opencart/opencart/pull/1619

--- upload/index.php
+++ upload/index.php
@@ -78,6 +78,11 @@ $registry->set('log', $log);
 function error_handler($errno, $errstr, $errfile, $errline) {
 	global $log, $config;
 
+	// error suppressed with @
+	if (error_reporting() === 0) {
+		return false;
+	}
+
 	switch ($errno) {
 		case E_NOTICE:
 		case E_USER_NOTICE:
