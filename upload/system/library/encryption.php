3358ce67bbbb0d41c3d15a1e5ac8369501f8ac4d
Derek <derek@garudacrafts.com>
2019-03-29 15:24:32 -0400

    Updated encryption class to use openssl with
    For more info, see https://www.php.net/manual/en/function.openssl-encrypt.php

--- upload/system/library/encryption.php
+++ upload/system/library/encryption.php
@@ -1,19 +1,27 @@
 <?php
 final class Encryption {
 	private $key;
+	private $cipher;
 	private $iv;
+	private $iv_length;
 
-	public function __construct($key) {
-		$this->key = hash('sha256', $key, true);
-		$this->iv = mcrypt_create_iv(32, MCRYPT_RAND);
-	}
+    public function __construct($key) {
+        $this->key = $key;
+		$this->cipher = 'aes-128-cbc';
+		$this->iv_length = openssl_cipher_iv_length($this->cipher);
+		$this->iv = openssl_random_pseudo_bytes($this->iv_length);
+    }
 
-	public function encrypt($value) {
-		return strtr(base64_encode(mcrypt_encrypt(MCRYPT_RIJNDAEL_256, $this->key, $value, MCRYPT_MODE_ECB, $this->iv)), '+/=', '-_,');
-	}
+    public function encrypt($value) {
+		return strtr(base64_encode($this->iv . openssl_encrypt($value, $this->cipher, hash('sha256', $this->key, true), 0, $this->iv)), '+/=', '-_,');
+    }
 
-	public function decrypt($value) {
-		return trim(mcrypt_decrypt(MCRYPT_RIJNDAEL_256, $this->key, base64_decode(strtr($value, '-_,', '+/=')), MCRYPT_MODE_ECB, $this->iv));
-	}
+    public function decrypt($value) {
+		$mixed = base64_decode(strtr($value, '-_,', '+/='));
+		$iv = substr($mixed, 0, $this->iv_length);
+		$encrypted = substr($mixed, $this->iv_length);
+
+		return trim(openssl_decrypt($encrypted, $this->cipher, hash('sha256', $this->key, true), 0, $iv));
+    }
 }
 ?>
