ca87393dd4314a114d83fab6899efe24e26e218b
John Sheppard <2039130+ADDCreative@users.noreply.github.com>
2019-12-23 16:33:05 +0000

    Stop bots causing non well formed numeric value

--- upload/catalog/controller/product/category.php
+++ upload/catalog/controller/product/category.php
@@ -22,7 +22,7 @@ class ControllerProductCategory extends Controller {
 		}
 
 		if (isset($this->request->get['page'])) {
-			$page = $this->request->get['page'];
+			$page = (int)$this->request->get['page'];
 		} else {
 			$page = 1;
 		}
--- upload/catalog/controller/product/product.php
+++ upload/catalog/controller/product/product.php
@@ -443,7 +443,7 @@ class ControllerProductProduct extends Controller {
 		$this->data['text_no_reviews'] = $this->language->get('text_no_reviews');
 
 		if (isset($this->request->get['page'])) {
-			$page = $this->request->get['page'];
+			$page = (int)$this->request->get['page'];
 		} else {
 			$page = 1;
 		}
